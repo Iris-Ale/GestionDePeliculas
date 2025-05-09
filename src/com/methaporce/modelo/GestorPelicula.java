b0c4afcec33bfcd9997b38c1c2538c18f294a1f1
Iris-Ale <irisalegro@gmail.com>
2025-05-08 19:10:17 -0600

    VERSION FINAL

--- src/com/methaporce/modelo/GestorPelicula.java
+++ src/com/methaporce/modelo/GestorPelicula.java
@@ -6,47 +6,48 @@ public class GestorPelicula {
 
     ArrayList<Pelicula> peliculas = new ArrayList<>();
 
-    public void agregarPelicula (Pelicula pelicula){
-        peliculas.add (pelicula);
+    public void agregarPelicula(Pelicula pelicula) {
+        peliculas.add(pelicula);
     }
 
-    public void eliminarPelicula(int id){
-        peliculas.removeIf(p->(p.getId() == id));
+    public void eliminarPelicula(int id) {
+        peliculas.removeIf(p -> (p.getId() == id));
     }
 
-    public void obtenerPeliculas (){
-        for (Pelicula pelicula : peliculas){
+    public void obtenerPeliculas() {
+        for (Pelicula pelicula : peliculas) {
             System.out.println(pelicula);
         }
     }
 
-    public ArrayList<Pelicula> obtenerPeliculasDisponibles() {
-        ArrayList<Pelicula> disponibles = new ArrayList <>();
-            for (Pelicula pe : peliculas){
-                if (pe.isDisponible()){
-                    disponibles.add (pe);
-                }
-        }
-            return disponibles;
-    }
-
-    public ArrayList<Pelicula> obtenerPeliculasNoDisponibles (){
-        ArrayList<Pelicula> disponibles = new ArrayList <>();
-        for (Pelicula pe : peliculas){
-            if (!pe.isDisponible()){
-                disponibles.add (pe);
+    public void obtenerPeliculasDisponibles() {
+        ArrayList<Pelicula> disponibles = new ArrayList<>();
+        for (Pelicula pe : peliculas) {
+            if (pe.isDisponible()) {
+                disponibles.add(pe);
+                System.out.println(pe);
             }
         }
-        return disponibles;
     }
 
-    public void marcarPeliculaDisponible(int id){
-        for (Pelicula p : peliculas){
-            if (p.getId() == id){
-                p.setDisponible (true);
+    public void obtenerPeliculasNoDisponibles() {
+        ArrayList<Pelicula> disponibles = new ArrayList<>();
+        for (Pelicula pe : peliculas) {
+            if (!pe.isDisponible()) {
+                disponibles.add(pe);
+                System.out.println(pe);
+            }
+        }
+    }
+
+    public void marcarPeliculaDisponible(int id) {
+        for (Pelicula p : peliculas) {
+            if (p.getId() == id) {
+                p.setDisponible(true);
                 break;
             }
         }
     }
 }
+
 
--- src/com/methaporce/vista/Main.java
+++ src/com/methaporce/vista/Main.java
@@ -25,18 +25,28 @@ public class Main {
         gestor.agregarPelicula (p4);
         gestor.agregarPelicula (p5);
 
+        //Obtener todas las peliculas
+        gestor.obtenerPeliculas ();
+
         //Comprobar peliculas disponibles
-        System.out.println("Peliculas disponibles hasta ahora: \n");
-        System.out.println(gestor.obtenerPeliculasDisponibles ());
+        System.out.println("\nPeliculas disponibles hasta ahora:");
+        gestor.obtenerPeliculasDisponibles ();
+
+        //Comprobar peliculas NO disponibles
+        System.out.println("\nPeliculas NO disponibles hasta ahora:");
+       gestor.obtenerPeliculasNoDisponibles ();
 
         //Establecer pelicula como disponible
         gestor.marcarPeliculaDisponible (5);
         System.out.println("\nMarcando pelicula como disponible...");
         System.out.println("Peliculas disponibles ahora: \n");
-        System.out.println(gestor.obtenerPeliculasDisponibles ());
+        gestor.obtenerPeliculasDisponibles ();
 
         //Eliminar una pelicula
-        System.out.println("Eliminando peliculas...");
+        System.out.println("\nEliminando peliculas...");
+        gestor.eliminarPelicula (2);
+        gestor.eliminarPelicula(4);
+        gestor.obtenerPeliculas ();
 
 
     }
